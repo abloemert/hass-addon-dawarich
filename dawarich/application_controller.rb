f4f4dbd2ac1449ed9f3dfa9361d94eaea874bfd2
abloemert <abloemert@gmail.com>
2025-05-07 22:18:25 +0200

    Initial Dawarich Home Assistant add-on

---
+++ dawarich/application_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  before_action :unread_notifications, :set_self_hosted_status
+
+  protected
+
+  def default_url_options
+    {}
+  end
+
+  def unread_notifications
+    return [] unless current_user
+
+    @unread_notifications ||= Notification.where(user: current_user).unread
+  end
+
+  def authenticate_admin!
+    return if current_user&.admin?
+
+    redirect_to root_path, notice: 'You are not authorized to perform this action.', status: :see_other
+  end
+
+  def authenticate_self_hosted!
+    return if DawarichSettings.self_hosted?
+
+    redirect_to root_path, notice: 'You are not authorized to perform this action.', status: :see_other
+  end
+
+  def authenticate_active_user!
+    return if current_user&.active_until&.future?
+
+    redirect_to root_path, notice: 'Your account is not active.', status: :see_other
+  end
+
+  def authenticate_non_self_hosted!
+    return unless DawarichSettings.self_hosted?
+
+    redirect_to root_path, notice: 'You are not authorized to perform this action.', status: :see_other
+  end
+
+  private
+
+  def set_self_hosted_status
+    @self_hosted = DawarichSettings.self_hosted?
+  end
+end
+
---
+++ dawarich/haos-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -ex
+
+export DATABASE_HOST=$(jq -r '.db_host' /data/options.json)
+export DATABASE_USERNAME=$(jq -r '.db_user' /data/options.json)
+export DATABASE_PASSWORD=$(jq -r '.db_pass' /data/options.json)
+export DATABASE_NAME=$(jq -r '.db_name' /data/options.json)
+
+export APPLICATION_HOSTS=$(jq -r '.hosts' /data/options.json)
+export RAILS_LOG_LEVEL=warn
+
+# Remove pre-existing puma/passenger server.pid
+rm -f $APP_PATH/tmp/pids/server.pid
+
+echo "Starting redis server..."
+redis-server &
+
+# Wait for the database to become available
+echo "⏳ Waiting for redis to be ready..."
+until redis-cli --raw incr ping; do
+  echo "Redis is unavailable - retrying..."
+  sleep 2
+done
+echo "✅ Redis is ready!"
+
+# Wait for the database to become available
+echo "⏳ Waiting for database "$DATABASE_NAME" to be ready on $DATABASE_USERNAME@$DATABASE_HOST..."
+until PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - retrying..."
+  sleep 2
+done
+echo "✅ PostgreSQL is ready!"
+
+# Run database migrations
+echo "PostgreSQL is ready. Running database migrations..."
+bundle exec rails db:migrate
+
+# Run data migrations
+echo "Running DATA migrations..."
+bundle exec rake data:migrate
+
+echo "Running seeds..."
+bundle exec rails db:seed
+
+bundle exec rails server -b :: &
+
+bundle exec sidekiq
