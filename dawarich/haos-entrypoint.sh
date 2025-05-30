b2934adfce39775e4eca1849b089a5b18cc3f949
abloemert <abloemert@gmail.com>
2025-05-30 11:04:10 +0200

    Improve logging

--- dawarich/haos-entrypoint.sh
+++ dawarich/haos-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 export DATABASE_HOST=$(jq -r '.db_host' /data/options.json)
 export DATABASE_USERNAME=$(jq -r '.db_user' /data/options.json)
@@ -12,36 +12,40 @@ export APPLICATION_HOSTS=$(jq -r '.hosts' /data/options.json)
 # Remove pre-existing puma/passenger server.pid
 rm -f $APP_PATH/tmp/pids/server.pid
 
-echo "Starting redis server..."
+echo "🚀 Starting redis server..."
 redis-server &
 
 # Wait for the database to become available
 echo "⏳ Waiting for redis to be ready..."
+sleep 2
 until redis-cli --raw incr ping; do
-  echo "Redis is unavailable - retrying..."
+  echo "⏳ Redis is unavailable - retrying..."
   sleep 2
 done
 echo "✅ Redis is ready!"
 
 # Wait for the database to become available
 echo "⏳ Waiting for database "$DATABASE_NAME" to be ready on $DATABASE_USERNAME@$DATABASE_HOST..."
+sleep 2
 until PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c '\q' 2>/dev/null; do
-  >&2 echo "Postgres is unavailable - retrying..."
+  >&2 echo "⏳ Postgres is unavailable - retrying..."
   sleep 2
 done
 echo "✅ PostgreSQL is ready!"
 
 # Run database migrations
-echo "PostgreSQL is ready. Running database migrations..."
+echo "🛠️ Running database migrations..."
 bundle exec rails db:migrate
 
 # Run data migrations
-echo "Running DATA migrations..."
+echo "🛠️ Running DATA migrations..."
 bundle exec rake data:migrate
 
-echo "Running seeds..."
+echo "🛠️ Running seeds..."
 bundle exec rails db:seed
 
+echo "🚀 Starting server..."
 bundle exec rails server -b :: &
 
+echo "🚀 Starting sidekiq..."
 bundle exec sidekiq
